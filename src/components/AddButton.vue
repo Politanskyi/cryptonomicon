2c7ddbe297e3c100e5f83e65682a486c68d64e15
Mike Politanskyi <politanskiy0072@gmail.com>
2021-08-03 17:37:24 +0300

    finish refactor

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,5 +1,6 @@
 <template>
 	<button type="button"
+			@click="$emit('click')"
 			:disabled="disabled"
 			class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
 			:class="{'opacity-50': disabled }">
@@ -25,6 +26,9 @@ export default {
 			required: false,
 			default: false
 		}
+	},
+	emits: {
+		click: null
 	}
 }
 </script>
